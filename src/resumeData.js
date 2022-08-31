2c69187a18470d3832df6205c744e081257146fb
wolhaiksongmazino <alrizal.luciel@gmail.com>
2022-08-31 19:32:52 +0700

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "imagebaseurl": "https://medium.com/@alrizal.luciel",
+    "name": "Al Rizal Aditya",
+    "role": "Full Stack Web Develover with a few years experience",
+    "linkedinId": "Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "",
+    "socialLinks": [{
+            "name": "linkedin",
+            "url": "https://www.linkedin.com/in/alrizal-aditya-7454a1229/",
+            "className": "fa fa-linkedin"
+        },
+        {
+            "name": "github",
+            "url": "https://github.com/wolhaiksongmazino",
+            "className": "fa fa-github"
+        },
+        {
+            "name": "skype",
+            "url": "https://twitter.com/AlrizalMazino",
+            "className": "fa fa-twitter"
+        }
+    ],
+    "aboutme": "I am currently working as Aircraft Mechanic Tester at PT Indonesian Aerospace and also Freelancer as Full Stack Web Developer",
+    "address": "Bandung , Indonesia",
+    "website": "https://medium.com/@alrizal.luciel",
+    "education": [{
+            "UniversityName": "University Of Jenderal Ahmad Yani",
+            "specialization": "Major in electrical instrument engineering",
+            "MonthOfPassing": "June",
+            "YearOfPassing": "2020",
+            // "Achievements": "Some Achievements"
+        },
+        {
+            "BootcampName": "Binar Academy",
+            "specialization": "Full Stack Web Developer",
+            "MonthOfPassing": "Nov",
+            "YearOfPassing": "2022",
+            // "Achievements": "Some Achievements"
+        }
+    ],
+    "work": [{
+            "CompanyName": "Indonesian Aerospace",
+            "specialization": "Aircraft Mechanic Tester",
+            // "MonthOfLeaving": "Jan",
+            // "YearOfLeaving": "2018",
+            // "Achievements": "Some Achievements"
+        },
+        //     // {
+        //     //     "CompanyName": "Some Company",
+        //     //     "specialization": "Some specialization",
+        //     //     "MonthOfLeaving": "Jan",
+        //     //     "YearOfLeaving": "2018",
+        //     //     "Achievements": "Some Achievements"
+        // }
+    ],
+    "skillsDescription": "Your skills here",
+    "skills": [{
+            "skillname": "HTML5"
+        },
+        {
+            "skillname": "CSS"
+        },
+        {
+            "skillname": "Reactjs"
+        }
+    ],
+    "portfolio": [{
+            "name": "project1",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/phone.jpg"
+        },
+        {
+            "name": "project2",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/project.jpg"
+        },
+        {
+            "name": "project3",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/project2.png"
+        },
+        {
+            "name": "project4",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/phone.jpg"
+        }
+    ],
+    "testimonials": [{
+            "description": "This is a sample testimonial",
+            "name": "Some technical guy"
+        },
+        {
+            "description": "This is a sample testimonial",
+            "name": "Some technical guy"
+        }
+    ]
+}
+
+export default resumeData
